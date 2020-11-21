6fac51a851e210519e2d40cc7ab6b12c3b903225
kristoferrobin66 <roblescristian991@gmail.com>
2020-11-21 14:50:11 -0600

    exception added when user tries to register the same course twice | login exceptions added

--- catedra/API_theam.py
+++ catedra/API_theam.py
@@ -23,6 +23,17 @@ db = mysql.connector.connect (
 
 cursor = db.cursor()
 
+def data_exists(table, column, data):
+    retData = False
+    preliminar = "error"
+    query = f"SELECT {column} FROM {table} WHERE {column}=\"{data}\""
+    cursor.execute(query)
+    for aux in cursor.fetchall():
+        preliminar = aux[0]
+        if preliminar != "error":
+            retData = True
+    return retData
+
 def login(data_base, cod, password):
     cursor = data_base.cursor()
     query = "SELECT COUNT(*) FROM usuario WHERE codigo = %s AND contrasenia = %s"
@@ -80,18 +91,21 @@ def getCourses(dataBase, code):
     return courses
 
 @app.route("/")
-@app.route("/login", methods = ['GET', 'POST'])
+@app.route("/login", methods = ['GET'])
 def accessUser ():
-    if request.method == 'GET':
-        codigo = request.args.get('codigo')
-        passw = request.args.get('contrasenia')
-        if (login(db,codigo,passw)):
-            return (jsonify(getUserData(db,codigo)))
-        else:
-           (jsonify({'code':'Dont exist user'}))
-    elif request.method == 'POST':
-        pass
-    return (jsonify({'code':'Error'}))
+    codigo = request.args.get('codigo')
+    passw = request.args.get('contrasenia')
+    if codigo == None:
+        return jsonify({'code': 'Error: No code or password provided'})
+    elif passw == None:
+        return jsonify({'code': 'Error: No code or password provided'})
+    elif data_exists("usuario", "codigo", codigo) == False:
+        return jsonify({'code':'Error: code or password are incorrect'})
+    elif data_exists("usuario", "contrasenia", passw) == False:
+        return jsonify({'code':'Error: code or password are incorrect'})
+    elif (login(db,codigo,passw)):
+        return jsonify(getUserData(db,codigo))
+    return (jsonify({'code':'User doesn\'t exist'}))
 
 @app.route('/circulares')
 @app.route('/circulares/<int:cant>')
@@ -109,66 +123,59 @@ def circulares(cant = 4):
     return jsonify(circulares)
 
 @app.route('/horario')
-@app.route('/horario/<int:cod>')
-def userCourses(cod = None):
-    if not cod:
-        return jsonify({'Code':'Missing user code'})
-    elif cod:
-        return jsonify(getCourses(db,cod))
-    else:
-        return jsonify({'Code':'Error'})
+@app.route('/horario/<int:codigo>')
+def userCourses(codigo=None):
+    if codigo == None:
+        return jsonify({'Code':'User code missing'})
+    elif codigo:
+        return jsonify(getCourses(db,codigo))
+    return jsonify({'Code':'Error'})
 
 '''
 Formats for request:
     GET -> /agenda?codigo=userCode&nrc=nrcRegis
 '''
 @app.route('/agenda', methods = ['GET'])
-def registrarMateriaAlumno():
+def registrarMateriaAlumno(codigo = None, nrc = None):
     codigo = request.args.get('codigo')
     nrc = request.args.get('nrc')
     cupDis = get_cupos_dis(nrc)
-    if cupDis == "error":
-        return jsonify({'code': 'Invalid NRC'})
-    if data_exists("usuario", "codigo", codigo) == False:
-        return jsonify({'code': 'Invalid code'})
     if not codigo and not nrc:
         return jsonify({'code' : 'Missing CODE and NRC'})
     if not codigo or not nrc:
         return jsonify({'code' : 'Missing argument', 'cod' : codigo, 'nrc' : nrc})
-    if request.method == "GET":
-        if (cupDis >= 0):
-            cupDis = cupDis - 1
-            query = "UPDATE materia SET cupDis=%s WHERE claseNRC=%s"
-            cursor.execute(query, (cupDis, nrc))
-            db.commit()
-            # se modifica tabla materiaAlumno
-            query = "INSERT INTO materiaAlumno (codigoAl, claseNrc) VALUES (%s, %s)"
-            cursor.execute(query, (codigo, nrc))
-            db.commit()
-            return jsonify({"code": "ok"})
-        # Si la materia ya cuenta con cupo negativo entonces se hay un error
-        return jsonify({"code": "error no space"})
-    else:
-        return jsonify({"code": "Error"})
+    if cupDis == "error":
+        return jsonify({'code': 'Invalid NRC'})
+    if data_exists("usuario", "codigo", codigo) == False:
+        return jsonify({'code': 'Invalid code'})
+    # Mensaje de error en caso de que el alumno quiera ingresar la misma materia más de una vez
+    if data_exists("materiaAlumno", "codigoAl", codigo) == True and\
+        data_exists("materiaAlumno", "claseNrc", nrc):
+        return jsonify({'code': 'User has already registered this course'})
+    if (cupDis >= 0):
+        cupDis = cupDis - 1
+        query = "UPDATE materia SET cupDis=%s WHERE claseNRC=%s"
+        cursor.execute(query, (cupDis, nrc))
+        db.commit()
+        # se modifica tabla materiaAlumno
+        query = "INSERT INTO materiaAlumno (codigoAl, claseNrc) VALUES (%s, %s)"
+        cursor.execute(query, (codigo, nrc))
+        db.commit()
+        return jsonify({"code": "ok"})
+    # Si la materia ya cuenta con cupo negativo entonces se hay un error
+    return jsonify({"code": "error no space"})
 
-def data_exists(table, column, data):
-    retData = False
-    preliminar = "error"
-    #query = "SELECT %s FROM %s WHERE %s = %s"
-    query = f"SELECT {column} FROM {table} WHERE {column}={data}"
-    cursor.execute(query)
-    for aux in cursor.fetchall():
-        preliminar = aux[0]
-        if preliminar != "error":
-            retData = True
-    return retData
+@app.route('/logout')
+def logout():
+    return
+
 
 def get_cupos_dis(nrc):
     # Dato preliminar, si el dato no cambia después de realizar la
     # query entonces la consulta falló    
     cupDis = "error"
     if not nrc:
-        raise Exception("No NRC provided")
+        return jsonify({'code': 'NRC Missing'})
     query = "SELECT cupDis from materia WHERE claseNrc=%s"
     cursor.execute(query, (nrc,))
     for aux in cursor.fetchall():
